e33af1a1e43fd808c4708d0a84347c6000055f67
Patricio Caram <patocaram@gmail.com>
2026-08-03 20:53:50 -0300

    Primera versión

---
+++ src/data/videos.js
@@ -0,0 +1,51 @@
+export const videos = [
+
+{
+    titulo:"UMA Energy",
+    categoria:"Industria",
+    descripcion:"Video institucional.",
+    thumb:"/thumbnails/1.jpg",
+    video:"/videos/1.mp4"
+},
+
+{
+    titulo:"Hitachi Energy",
+    categoria:"Industria",
+    descripcion:"Comunicación corporativa.",
+    thumb:"/thumbnails/2.jpg",
+    video:"/videos/2.mp4"
+},
+
+{
+    titulo:"Proyecto 3",
+    categoria:"Empresas",
+    descripcion:"Video institucional.",
+    thumb:"/thumbnails/3.jpg",
+    video:"/videos/3.mp4"
+},
+
+{
+    titulo:"Proyecto 4",
+    categoria:"Productos",
+    descripcion:"Producción audiovisual.",
+    thumb:"/thumbnails/4.jpg",
+    video:"/videos/4.mp4"
+},
+
+{
+    titulo:"Proyecto 5",
+    categoria:"Historias",
+    descripcion:"Storytelling.",
+    thumb:"/thumbnails/5.jpg",
+    video:"/videos/5.mp4"
+},
+
+{
+    titulo:"Proyecto 6",
+    categoria:"Industria",
+    descripcion:"Video corporativo.",
+    thumb:"/thumbnails/6.jpg",
+    video:"/videos/6.mp4"
+}
+
+];
